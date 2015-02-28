928f168c125456e63454bfa87bd4d5272a1cd330
Sinead Urisohn <sinzara611@gmail.com>
2015-02-28 03:49:28 +0200

    Wrote nextLevel method in TreeUtils

--- pracOne/TreeUtils.java
+++ pracOne/TreeUtils.java
@@ -13,22 +13,34 @@ public class TreeUtils{
 	*/
 	static List<BinaryTreeNode> levelZero(BinaryTreeNode tNode) 
 	{
-		
+		LinkedList<BinaryTreeNode> list = new LinkedList<BinaryTreeNode>();
+		list.push(tNode);
+		return list;
 	}
 	/**
 	*Given a list of nodes, obtain the next level.
 	*/
 	static List<BinaryTreeNode> nextLevel(List<BinaryTreeNode> level) 
 	{
+		LinkedList<BinaryTreeNode> list = new LinkedList<BinaryTreeNode>();
 
+		for (BinaryTreeNode node : level) {
+			if (node.hasLeft()) list.push(node.getLeft());
+			else list.push(BinaryTreeNode.EMPTY_NODE);
+
+			if (node.hasRight()) list.push(node.getRight());
+			else list.push(BinaryTreeNode.EMPTY_NODE);
+		}
+
+		return list;
 	}
 	/**
 	*Determine whether one tree node structure is similar (has the same structure) as another.
 	*/
-	static boolean similar(BinaryTreeNode treeStructOne, BinaryTreeNode treeStructTwo) 
-	{
+	// static boolean similar(BinaryTreeNode treeStructOne, BinaryTreeNode treeStructTwo) 
+	// {
 
-	}
+	// }
 
 
 }
