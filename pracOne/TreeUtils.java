004b3140b7c9174c9fad739c0f274cd38ab4cd70
Sinead Urisohn <sinzara611@gmail.com>
2015-02-28 03:38:19 +0200

    Wrote isPlaceHolder method in TreeUtils

---
+++ pracOne/TreeUtils.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class TreeUtils{
+	/**
+	*Determine whether node is a place holder
+	*/
+	public boolean isPlaceHolder(BinaryTreeNode node) 
+	{
+		return node == BinaryTreeNode.EMPTY_NODE;
+	}
+	/**
+	*Obtain a list containing the root node of the given structure i.e.
+	*/
+	static List<BinaryTreeNode> levelZero(BinaryTreeNode tNode) 
+	{
+		
+	}
+	/**
+	*Given a list of nodes, obtain the next level.
+	*/
+	static List<BinaryTreeNode> nextLevel(List<BinaryTreeNode> level) 
+	{
+
+	}
+	/**
+	*Determine whether one tree node structure is similar (has the same structure) as another.
+	*/
+	static boolean similar(BinaryTreeNode treeStructOne, BinaryTreeNode treeStructTwo) 
+	{
+
+	}
+
+
+}
