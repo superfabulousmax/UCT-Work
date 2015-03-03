9c7dc57925e4f9729de8e946375a880fdbb96984
Sinead Urisohn <sinzara611@gmail.com>
2015-03-03 23:24:05 +0200

    Added additional comments to TreeMain.java and fixed printing aesthetics

--- pracOne/TreeMain.java
+++ pracOne/TreeMain.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.io.*;
 /**
 *Main class that does all the coordination of prints tree and tests similarity
-*@author
+*@author Sinead Urisohn
 *@version 01/03/2015
 */
 public class TreeMain{
@@ -17,10 +17,10 @@ public class TreeMain{
         System.out.print("Enter a comma separated list of numbers for tree two: ");
         String line2=scanner.nextLine();
         Scanner numbersOfLine2 = new Scanner(line2).useDelimiter(",");
-        //System.out.println(line1+"\n"+line2);
+        
         SimpleBST tree1 = new  SimpleBST();
         
-       // System.out.println((int)numbersOfLine1.nextInt() instanceof Integer);
+       // insert into trees
         while (numbersOfLine1.hasNextInt()){
             tree1.insert(numbersOfLine1.nextInt());
         }
@@ -33,10 +33,12 @@ public class TreeMain{
         SimpleBST.print(tree1, new SimpleTreeWriterImpl(System.out));
         System.out.println("Tree two:");
         SimpleBST.print(tree2, new SimpleTreeWriterImpl(System.out));
+
+        //compare similarity
         if(tree1.similar(tree2))
-            System.out.print("The trees are similar");
+            System.out.println("\nThe trees are similar");
         else
-            System.out.print("The trees are not similar");
+            System.out.println("\nThe trees are not similar");
 
         //write trees to textfiles
         try{
