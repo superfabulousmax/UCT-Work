a14dd983e46dc8e168b7427ca4cc85f224e30937
Sinead Urisohn <sinzara611@gmail.com>
2015-03-03 18:56:59 +0200

    Completed implementing JUnit test

--- pracOne/TestTreeUtils.java
+++ pracOne/TestTreeUtils.java
@@ -16,24 +16,91 @@ public class TestTreeUtils{
 	private int [] array3 ={2,9,7,8};
 
 
-	private int [] array4 = {17,11,9,2,3,1};
+	private int [] array4 = {9,8,7,14,11,22};
 
 
 	private int [] array5 = {8,7,6,15,12,23};
 
 	private int [] array6 = {8,7,6,34,12,23};
+	private int [] array7= {1,10,6,7};
 	SimpleBST tree1 = new  SimpleBST();
+	SimpleBST tree2 = new  SimpleBST();
+	
+	SimpleBST tree3 = new  SimpleBST();
+	
+	SimpleBST tree4 = new  SimpleBST();
+	SimpleBST tree5 = new  SimpleBST();
+	
+	SimpleBST tree6 = new  SimpleBST();
+	SimpleBST tree7 = new  SimpleBST();
+	@Before
+	public void setup(){
+	
+
+	
+		for(int i=0;i<array1.length;i++)
+		{
+			tree1.insert(i);
+		}
+
+		for(int i=0;i<array2.length;i++)
+		{
+			tree2.insert(i);
+		}
+		for(int i=0;i<array3.length;i++)
+		{
+			tree3.insert(i);
+		}
+		for(int i=0;i<array4.length;i++)
+		{
+			tree4.insert(i);
+		}
+		for(int i=0;i<array5.length;i++)
+		{
+			tree5.insert(i);
+		}
+		for(int i=0;i<array6.length;i++)
+		{
+			tree6.insert(i);
+		}
+		for(int i=0;i<array7.length;i++)
+		{
+			tree7.insert(i);
+		}
+	}//end setup method
+	
 	BinaryTreeNode placeholder = BinaryTreeNode.EMPTY_NODE;//if tree is incomplete
 
 
-	@test
+	@Test
 	public void testPlaceHolder(){
-		assertTrue(TreeUtils.isPlaceHolder(placeholder));
+		assertTrue("This should be true",TreeUtils.isPlaceHolder(placeholder));
 	}
 
 
 	@Test
-	public void testSimilar(){
+	public void testSimilar1(){
+		assertTrue("This should be true", tree1.similar(tree3));
+	}
+	@Test
+	public void testSimilar2(){
+		assertTrue("This should be false", tree1.similar(tree2));
+	}
+	@Test
+	public void testSimilar3(){
+		assertTrue("This should be false", tree3.similar(tree4));
+	}
+	@Test
+	public void testSimilar4(){
+		assertTrue("This should be false", tree5.similar(tree6));
+	}
+	@Test
+	public void testSimilar5(){
+		assertTrue("This should be true", tree4.similar(tree5));
+	}
+	@Test
+	public void testSimilar6(){
+		assertTrue("This should be true", tree3.similar(tree7));
 
 	}
 
